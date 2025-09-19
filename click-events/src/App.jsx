deb2b55475454cde327425dc09137c8eadf54f7e
Matthew Boyer <lyfsux@MacBookPro.lan>
2025-09-19 10:57:54 -0500

    Setup Vite, initial setup

---
+++ click-events/src/App.jsx
@@ -0,0 +1,16 @@
+// click event = An interaction when a user clicks on a specific element.
+// Respond to ckicks by passing a callback to the onClick event handler.
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      
+    </>
+  )
+}
+
+export default App
